f133342ac626f67c8d3f5ca195d04d41e9540b79
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2024-09-09 22:11:05 +0200

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,22 @@
-console.log('Form ');
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+  email: '',
+  message: '',
+};
+form.email.value = formData.email;
+form.message.value = formData.message;
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!form.email.value || !form.message.value) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  event.target.reset();
+});
